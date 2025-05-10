69eb111c1bc245e0e2698ac4fb6cbd233283091c
Mihaylov1VV <vitalii.mihailov1995@gmail.com>
2025-05-10 12:50:22 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise2;
+
+class Program
+{
+    //Задание 2. Объявите и инициализируйте переменную следующего вида: 
+    //переменная для обозначение возраста человека, имеющая значение вашего возраста.
+    //переменная температуры за окном
+    //переменная логического типа, обозначающая принадлженость к женскому полу.
+    //
+    static void Main(string[] args)
+    {
+        uint you_age = 29;
+        Console.WriteLine(you_age);
+        string name = "Vitaliy";
+        Console.WriteLine(name);
+        int temp = 10;
+        Console.WriteLine(temp);
+        bool woman = false;
+        Console.WriteLine(woman);
+    }
+}
